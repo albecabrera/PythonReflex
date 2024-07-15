f15861eb4f39f9af3b7b557b24db3a1c98c0d42b
albecabrera <albe.cabrera@yahoo.de>
2024-07-15 22:47:37 +0200

    commit -m "change2"

--- PythonReflex/link_bio.py
+++ PythonReflex/link_bio.py
@@ -1,11 +1,18 @@
 import reflex as rx
 
-class State(rx.state):
-  pass
+class State(rx.State):
+pass
 
-  def index() -> rx.Component:
-    return rx.text("Hola Reflex!")
-  
-  app = rx.App
-  app.add_page(index)
-  app._compile()
+def index() -> rx. Component:
+  return rx.hstack(
+    rx.text(
+      "ACabrera",
+      height="40px",
+            ),
+      position="sticky",
+  )
+
+
+app = rx. App()
+app. add_page(index)
+app. compile()
